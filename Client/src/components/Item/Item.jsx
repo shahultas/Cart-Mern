de258b21cefd2c19668bb34b5bda283b0b01db1b
SHANIB C K <ckshanib8@gmail.com>
2024-02-29 10:02:30 +0530

    item listed and its responsived

--- Client/src/components/Item/Item.jsx
+++ Client/src/components/Item/Item.jsx
@@ -4,11 +4,11 @@ import "./Item.css";
 const Item = (props) => {
   return (
     <div className="item">
-      <img src={props.image} alt="" />
+      <img src={props.image} />
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-proces-new">{props.new_price}</div>
-        <div className="item-price-old">{props.old_price}</div>
+        <div className="item-proces-new">${props.new_price}</div>
+        <del className="item-price-old">${props.old_price}</del>
       </div>
     </div>
   );
