f8c089c8722e7b10b36b5e45c9e0bb18781e289e
SHANIB C K <ckshanib8@gmail.com>
2024-02-29 09:15:46 +0530

    😊😊

---
+++ Client/src/components/Popular/Popular.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Popular.css";
+import data_product from "./../../assets/data";
+
+const Popular = () => {
+  return <div className="popular"></div>;
+};
+
+export default Popular;
