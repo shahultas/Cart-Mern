737f5e72665e61e7f38f571f98931782695cf5ea
SHANIB C K <ckshanib8@gmail.com>
2024-03-02 19:16:14 +0530

    newsletter created

--- Client/src/pages/Shop.jsx
+++ Client/src/pages/Shop.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/Hero/Hero";
 import Popular from "../components/Popular/Popular";
 import Offers from "../components/Offers/Offers";
 import NewCollection from "../components/NewCollection/NewCollection";
+import NewsLetter from "../components/NewsLetter/NewsLetter";
 
 const Shop = () => {
   return (
@@ -11,6 +12,7 @@ const Shop = () => {
       <Popular />
       <Offers />
       <NewCollection />
+      <NewsLetter />
     </div>
   );
 };
