64950505836e52d1c2419365ea9db79c9eea9257
SHANIB C K <ckshanib8@gmail.com>
2024-03-03 18:17:16 +0530

    shopcategory page designed completed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,6 +7,9 @@ import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import LoginSignup from "./pages/LoginSignup";
 import Footer from "./components/Footer/Footer";
+import men_banner from "./assets/banner_mens.png";
+import women_banner from "./assets/banner_women.png";
+import kid_banner from "./assets/banner_kids.png";
 
 function App() {
   return (
@@ -15,9 +18,18 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category="men" />} />
-          <Route path="/womens" element={<ShopCategory category="women" />} />
-          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} category="kid" />}
+          />
           <Route path="/product" element={<Products />}>
             <Route path=":productId" element={<Products />} />
           </Route>
